59f6bc7f2338df752b5ad99b59592e4f38c3d657
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2021-06-24 23:52:10 -0400

    improve deployment + gemini layout

--- _deploy.sh
+++ _deploy.sh
@@ -3,24 +3,16 @@
 # If a command fails then the deploy stops
 set -e
 
-printf "\033[0;32mDeploying updates to ipfs...\033[0m\n"
+printf "\033[0;32mDeploying updates to ipfs for $DOMAIN...\033[0m\n"
 
+DOMAIN="karmanyaah.malhotra.cc"
 DIR="./_site"
-KEY="karmanyaah.malhotra.cc"
-LOC="/website/$KEY"
 
-ipfs add "$DIR" -rp --pin=false
+BW_ENTRY=cloudflare_malhotra.cc_token
+LOC="/website/$DOMAIN"
 
-CID=$(ipfs add "$DIR" -rQ --pin=false)
-KEY_VALUE=$(ipfs key list -l | grep $KEY | cut -d ' ' -f1 | xargs ipfs name resolve) # resolves own key
-echo $CID $KEY_VALUE
-if [[ "/ipfs/$CID" == $KEY_VALUE ]]; then 
-	echo "ALREADY PUBLISHED"
-else
-	echo PUBLISHING
-	ipfs name publish -k $KEY $CID
-fi
-
+CID=$(ipfs add "$DIR" -rpQ --pin=false)
+echo added to $CID
 
 ipfs files mkdir -p "$LOC"
 if ipfs files ls -l "$LOC" | grep -q $CID; then
@@ -30,3 +22,17 @@ else
 	ipfs files cp /ipfs/$CID "$LOC/$(date +%F_%T)"
 fi
 
+RECORD_DOMAIN="_dnslink.$KEY"
+
+# password is token and custom fields called zone and for each record
+SECRETS="$(rbw get $BW_ENTRY --full)"
+CLOUDFLARE_TOKEN="$(echo "$SECRETS" | head -n 1)"
+ZONE_ID="$(echo "$SECRETS" | grep -i zone | cut -d ' ' -f 2 )"
+RECORD_ID="$(echo "$SECRETS" | grep -i $RECORD_DOMAIN | cut -d ' ' -f 2)"
+
+function curll {
+	curl -s -H "Authorization: Bearer $CLOUDFLARE_TOKEN" -w '\n' -H "Content-Type:application/json" $@
+}
+
+echo -n "dns $ZONE_ID/$RECORD_ID success: "
+curll "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" -X PATCH --data "{\"content\":\"dnslink=/ipfs/$CID\"}" | jq '.success'
