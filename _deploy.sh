8af00ffae6e80f04c384a94993056923752a1229
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2021-10-17 17:09:22 -0500

    unblocking stuff

--- _deploy.sh
+++ _deploy.sh
@@ -3,13 +3,18 @@
 # If a command fails then the deploy stops
 set -e
 
-printf "\033[0;32mDeploying updates to ipfs for $DOMAIN...\033[0m\n"
 
-DOMAIN="karmanyaah.malhotra.cc"
+RECORD_ZONE="malhotra.cc"
+DOMAIN="karmanyaah.$RECORD_ZONE"
 DIR="./_site"
 
 BW_ENTRY=cloudflare_malhotra.cc_token
+
+#####
+
 LOC="/website/$DOMAIN"
+
+printf "\033[0;32mDeploying updates to ipfs for $DOMAIN...\033[0m\n"
 
 CID=$(ipfs add "$DIR" -rpQ --pin=false)
 echo added to $CID
@@ -25,14 +30,14 @@ fi
 RECORD_DOMAIN="_dnslink.$DOMAIN"
 
 # password is token and custom fields called zone and for each record
-SECRETS="$(rbw get $BW_ENTRY --full)"
-CLOUDFLARE_TOKEN="$(echo "$SECRETS" | head -n 1)"
-ZONE_ID="$(echo "$SECRETS" | grep -i zone | cut -d ' ' -f 2 )"
-RECORD_ID="$(echo "$SECRETS" | grep -i $RECORD_DOMAIN | cut -d ' ' -f 2)"
+if [ -z "$CLOUDFLARE_TOKEN" ]; then CLOUDFLARE_TOKEN="$(rbw get $BW_ENTRY || continue)"; fi
 
 function curll {
 	curl -s -H "Authorization: Bearer $CLOUDFLARE_TOKEN" -w '\n' -H "Content-Type:application/json" $@
 }
 
+ZONE_ID=$(curll "https://api.cloudflare.com/client/v4/zones?name=$RECORD_ZONE"  | jq ".result[0].id" -r)
+RECORD_ID=$(curll "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?name=$RECORD_DOMAIN"  | jq ".result[0].id" -r)
 echo -n "dns $ZONE_ID/$RECORD_ID success: "
 curll "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" -X PATCH --data "{\"content\":\"dnslink=/ipfs/$CID\"}" | jq '.success'
+
